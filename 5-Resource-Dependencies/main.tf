ca8b0a3ec282f0a6a1694e3551235989aa8790ff
inspired123 <68370169+inspired123@users.noreply.github.com>
2020-11-13 19:19:34 +0530

    Update main.tf

--- 5-Resource-Dependencies/main.tf
+++ 5-Resource-Dependencies/main.tf
@@ -1,27 +1,43 @@
+locals {
+  system = "CSkillsApp"
+}
+
+
 provider "azurerm" {
   version = "1.38.0"
 }
 
 #create resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-MyFirstTerraform"
-    location = "westus2"
+    name     = "image"
+    location = "South Central US"
     tags      = {
-      Environment = "Terraform Demo"
+      Environment = local.system
     }
 }
 
+# Locate the existing custom/golden image
+data "azurerm_image" "search" {
+  name                = "image"
+  resource_group_name = "image"
+}
+
+output "image_id" {
+  value = "/subscriptions/cf0bcec9-631e-4dc9-8d08-807fa41bbe6e/resourceGroups/image/providers/Microsoft.Compute/images/image"
+}
+
+
 #Create virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "vnet-dev-westus2-001"
+    name                = "vnet-${local.system}-westus2-001"
     address_space       = ["10.0.0.0/16"]
-    location            = "westus2"
+    location            = "South Central US"
     resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "snet-dev-westus2-001 "
+  name                 = "snet-${local.system}-westus2-001 "
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "10.0.0.0/24"
@@ -29,8 +45,8 @@ resource "azurerm_subnet" "subnet" {
 
 # Create public IP
 resource "azurerm_public_ip" "publicip" {
-  name                = "pip-vmterraform-dev-westus2-001"
-  location            = "westus2"
+  name                = "pip-${local.system}-dev-westus2-001"
+  location            = "South Central US"
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
 }
@@ -39,7 +55,7 @@ resource "azurerm_public_ip" "publicip" {
 # Create network security group and rule
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-sshallow-001 "
-  location            = "westus2"
+  location            = "South Central US"
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
@@ -57,13 +73,13 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                      = "nic-01-vmterraform-dev-001 "
-  location                  = "westus2"
+  name                      = "nic-01-${local.system}-dev-001 "
+  location                  = "South Central US"
   resource_group_name       = azurerm_resource_group.rg.name
   network_security_group_id = azurerm_network_security_group.nsg.id
 
   ip_configuration {
-    name                          = "niccfg-vmterraform"
+    name                          = "niccfg-${local.system}"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
@@ -72,33 +88,29 @@ resource "azurerm_network_interface" "nic" {
 
 # Create virtual machine
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "vmterraform"
-  location              = "westus2"
+  name                  = "image"
+  location              = "South Central US"
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_B1s"
+  vm_size               = "Standard_DS2_v2"
 
   storage_os_disk {
     name              = "stvmpmvmterraformos"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
+    managed_disk_type = "Standard_LRS"
   }
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04.0-LTS"
-    version   = "latest"
+    id = "${data.azurerm_image.search.id}"
   }
 
   os_profile {
-    computer_name  = "vmterraform"
+    computer_name  = "image"
     admin_username = "terrauser"
     admin_password = "Password1234!"
   }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
+os_profile_windows_config {
   }
 }
